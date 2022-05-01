fbbde642e342fb2b8661563176e738b652ffd44a
Draqode <Boomjay008@gmail.com>
2022-05-01 03:25:05 +0100

    gege

--- pages/actors/index.js
+++ pages/actors/index.js
@@ -5,7 +5,7 @@ import {
 } from '@chakra-ui/react';
 import { RiSearch2Line } from 'react-icons/ri';
 import Navbar from '../../components/Navbar';
-//import Axios from 'axios';
+import Axios from 'axios';
 import Card from '../../components/Card';
 
 
@@ -48,7 +48,7 @@ export default function Shows({Data}) {
 
 
 export async function getStaticProps() {
-  const mov = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=${process.env.PRIVATE_KEY}&language=en-US&page=1`)
+  const mov = await Axios.get(`https://api.themoviedb.org/3/person/popular?api_key=${process.env.PRIVATE_KEY}&language=en-US&page=1`)
   const response = await mov.data.results
 
   return{
