fbe6194c236bf0cea3d615f8e4d00080094682a8
Draqode <Boomjay008@gmail.com>
2022-05-06 12:27:04 +0100

    feat: changed the search layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import { Box, Flex, Tab, TabList, Link,TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react';
+import { Box, Flex, Tab, TabList, Link,TabPanel,Heading, TabPanels, Tabs, Text, InputGroup, InputLeftElement, Input,Modal,ModalOverlay, ModalContent, ModalBody, ModalHeader, ModalFooter, useDisclosure, } from '@chakra-ui/react';
 import Card from "../components/Card";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Axios from 'axios'
 import NextLink from 'next/link';
 import { useState } from 'react';
+import { RiSearch2Line } from 'react-icons/ri'
 
 
 
 export default function Home({ movieData, showsData, allData }) {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [all, setAll] = useState(allData);  
   const [val, setVal] = useState("");
   async function searchFetch() {
@@ -30,7 +33,48 @@ export default function Home({ movieData, showsData, allData }) {
     <Box bgImage='url("Background.svg")' w='100%' bgColor='#121829'>
       <Navbar />
       <Box px={['20px','20px','70px']}>
-        <Hero />
+        {/* <Hero /> */}
+        <Box w={['','','500px']} pt={['80px','90px', '100px', '120px']} pb='20px' >
+      <Heading color='#EBEEF5' fontWeight='600' fontSize='64px' textAlign={['justify', 'justify', 'left']}> Flix</Heading>
+      <Text fontSize='16px' fontWeight='400' color='#8E95A9'> A web app where you can search for info about your favorite movies, tv shows and actors 😉. If there's a particular movie that you can't find, shoot me a <Text as='span' color='#9C92F8'>mail</Text> </Text>
+        {/* <Box as='button' variant='' px='10px' onClick={() => onOpen()} borderRadius='12px' d='flex'h='64px' w={['100%','100%','348px']} mt='24px' alignItems='center' justifyContent='unset' bg='#0000001A' border='1px solid #323B54'>
+          <RiSearch2Line color='#475069' fontSize='24px' pl='10px' />
+          <Flex color='#475069' ml='8px' fontWeight='400' fontSize='14px'>
+            Search Movies or TV Shows...
+          </Flex> 
+      </Box> */}
+      <>
+        
+      <Modal bg='green' preserveScrollBarGap={true}  size='xl' isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent bg='transparent'>
+          <ModalHeader>
+            
+          </ModalHeader>
+          <ModalBody>
+            <InputGroup  >
+              <InputLeftElement
+                pt='20px' 
+              children={<RiSearch2Line color='#9C92F8'  fontSize='24px' />}/>
+              <Input  outline='0' 
+                _hover={{
+                borderColor:'#323B54'
+                }}
+                _placeholder={{color:'#475069', fontSize:'16px', fontWeight: '400' }}
+                border='1px solid #323B54'
+                bg='#0000001A'
+                focusBorderColor='none'
+                  color='white' h='64px' placeholder='Search Movies or TV Shows' fontSize='20px' fontWeight='' borderColor='#323B54' />
+              </InputGroup>
+          </ModalBody>
+          <ModalFooter>
+
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+        
+      </>
+    </Box>
         <Box mt='60px' >
           <Tabs
             variant='unstyled'>
